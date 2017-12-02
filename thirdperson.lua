32012e33f2b288574eea3475382716770564522a
Sebastian Gabl <sebastian.gabl@gmx.at>
2017-12-02 11:51:31 +0100

    Save first person unit in mod data
    Instead of saving it in the third person unit base, it's now saved
    in the mod data for easier access.
    Also improved and restructured some code.

--- thirdperson.lua
+++ thirdperson.lua
@@ -4,6 +4,7 @@ if not ThirdPerson then
   ThirdPerson.mod_path = ModPath
   ThirdPerson.save_path = SavePath
   ThirdPerson.unit = nil
+  ThirdPerson.fp_unit = nil
   ThirdPerson.delayed_events = {}
   ThirdPerson.settings = {
     cam_x = 80,
@@ -33,9 +34,9 @@ if not ThirdPerson then
     local rot = player_movement:m_head_rot()
     local unit_name = Idstring(tweak_data.blackmarket.characters[player_peer:character_id()].npc_unit:gsub("(.+)/npc_", "%1/player_") .. "_husk")
     
+    self.fp_unit = player
     self.unit = alive(self.unit) and self.unit or World:spawn_unit(unit_name, pos, rot)
     
-    self.unit:base()._first_person_unit = player
     player:base().pre_destroy = function (self, ...)
       if alive(ThirdPerson.unit) then
         World:delete_unit(ThirdPerson.unit)
@@ -52,33 +53,43 @@ if not ThirdPerson then
     local look_vec_modified = Vector3()
     self.unit:movement().update = function (self, ...)
       HuskPlayerMovement.update(self, ...)
-      local fp_unit = self._unit:base()._first_person_unit
-      if alive(fp_unit) then
+      if alive(ThirdPerson.fp_unit) then
         -- correct aiming direction so that lasers are approximately the same in first and third person
-        mvector3.set(look_vec_modified, fp_unit:camera():forward())
-        mvector3.rotate_with(look_vec_modified, Rotation(fp_unit:camera():rotation():z(), 1) * Rotation(fp_unit:camera():rotation():x(), -0.5))
+        mvector3.set(look_vec_modified, ThirdPerson.fp_unit:camera():forward())
+        mvector3.rotate_with(look_vec_modified, Rotation(ThirdPerson.fp_unit:camera():rotation():z(), 1) * Rotation(ThirdPerson.fp_unit:camera():rotation():x(), -0.5))
         self:set_look_dir_instant(look_vec_modified)
       end
     end
     self.unit:movement().set_position = function (self, pos)
-      local fp_unit = self._unit:base()._first_person_unit
-      if alive(fp_unit) and fp_unit:camera():first_person() then
+      if alive(ThirdPerson.fp_unit) and ThirdPerson.fp_unit:camera():first_person() then
         self._unit:set_position(Vector3(0, 0, -10000))
       else
-        HuskPlayerMovement.set_position(self, alive(fp_unit) and fp_unit:movement():m_pos() or pos)
+        HuskPlayerMovement.set_position(self, alive(ThirdPerson.fp_unit) and ThirdPerson.fp_unit:movement():m_pos() or pos)
       end
     end
     self.unit:movement().set_need_assistance = function (self) end
     self.unit:movement().set_need_revive = function (self) end
     self.unit:movement().set_head_visibility = function (self, visible)
-      if not self._plr_head_mesh_obj then
-        local char_name = managers.criminals.convert_old_to_new_character_workname(managers.criminals:character_name_by_unit(self._unit))
-        self._plr_head_mesh_obj = char_name and self._unit:get_object(Idstring("g_head_" .. char_name))
+      -- needs work, doesnt get all criminals heads
+      local char_name = managers.criminals.convert_old_to_new_character_workname(managers.criminals:character_name_by_unit(self._unit))
+      local head_obj = char_name and self._unit:get_object(Idstring("g_head_" .. char_name))
+      if head_obj then
+        head_obj:set_visibility(visible)
       end
-      if self._plr_head_mesh_obj then
-        self._plr_head_mesh_obj:set_visibility(visible)
+      local neck_armor_obj = self._unit:get_object(Idstring("g_vest_neck"))
+      if neck_armor_obj then
+        neck_armor_obj:set_visibility(visible and neck_armor_obj:visibility())
       end
       self._unit:inventory():set_mask_visibility(visible and self._unit:inventory()._mask_visibility)
+    end
+    self.unit:movement().update_armor = function (self)
+      if alive(ThirdPerson.fp_unit) then
+        local player_peer = ThirdPerson.fp_unit:network():peer()
+        player_peer._unit = self._unit
+        player_peer._equipped_armor_id = "level_1"
+        player_peer:_update_equipped_armor()
+        player_peer._unit = ThirdPerson.fp_unit
+      end
     end
     self.unit:inventory().set_mask_visibility = function (self, state)
       HuskPlayerInventory.set_mask_visibility(self, not ThirdPerson.settings.immersive_first_person and state)
@@ -91,9 +102,9 @@ if not ThirdPerson then
     self.unit:inventory():set_melee_weapon(player_peer:melee_id(), true)
     self.unit:damage():run_sequence_simple(managers.blackmarket:character_sequence_by_character_id(player_peer:character_id(), player_peer:id()))
     self.unit:movement():set_character_anim_variables()
+    self.unit:movement():update_armor()
     self.unit:movement():set_head_visibility(not ThirdPerson.settings.immersive_first_person)
     self.unit:contour():remove("teammate")
-    self.unit:movement():set_attention_updator(function () end)
     
     -- Call delayed events
     local handler = managers.network and managers.network._handlers and managers.network._handlers.unit
@@ -113,11 +124,6 @@ if not ThirdPerson then
         self.unit:damage():run_sequence_simple(sequence)
       end
     end
-    
-    player_peer._unit = self.unit
-    player_peer._equipped_armor_id = "level_1"
-    player_peer:_update_equipped_armor()
-    player_peer._unit = player
     
     player:camera():set_third_person()
     
@@ -212,13 +218,13 @@ if RequiredScript == "lib/units/beings/player/playercamera" then
   function PlayerCamera:set_first_person()
     self:camera_unit():base():set_target_tilt(self:camera_unit():base()._fp_target_tilt or 0)
     self._vp:set_camera(self._camera_object)
-    ThirdPerson.unit:movement():set_position()
+    ThirdPerson.unit:movement():set_position(Vector3())
   end
   
   function PlayerCamera:set_third_person()
     self:camera_unit():base():set_target_tilt(0)
     self._vp:set_camera(self._tp_camera_object)
-    ThirdPerson.unit:movement():set_position()
+    ThirdPerson.unit:movement():set_position(Vector3())
   end
   
   function PlayerCamera:first_person()
@@ -346,8 +352,8 @@ if RequiredScript == "lib/managers/criminalsmanager" then
     if k:find("_by_unit") then
       local orig = v
       CriminalsManager[k] = function (self, unit)
-        if alive(ThirdPerson.unit) and unit == ThirdPerson.unit and alive(unit:base()._first_person_unit) then
-          return orig(self, unit:base()._first_person_unit)
+        if unit == ThirdPerson.unit and alive(ThirdPerson.unit) and alive(ThirdPerson.fp_unit) then
+          return orig(self, ThirdPerson.fp_unit)
         end
         return orig(self, unit)
       end
@@ -386,6 +392,7 @@ if RequiredScript == "lib/network/base/networkpeer" then
     if self == managers.network:session():local_peer() then
       ThirdPerson:setup_unit()
     end
+    return unit
   end
 
 end
