054ef197c733fdc177de8c0306c47064f0a16f2a
Sebastian Gabl <sebastian.gabl@gmx.at>
2017-12-21 16:22:38 +0100

    Check if it's the actual FP unit

--- lua/playerstandard.lua
+++ lua/playerstandard.lua
@@ -1,7 +1,7 @@
 local _start_action_steelsight_original = PlayerStandard._start_action_steelsight
 function PlayerStandard:_start_action_steelsight(...)
   _start_action_steelsight_original(self, ...)
-  if self._state_data.in_steelsight and ThirdPerson.settings.first_person_on_steelsight then
+  if self._unit == ThirdPerson.fp_unit and self._state_data.in_steelsight and ThirdPerson.settings.first_person_on_steelsight then
     self._unit:camera():set_first_person()
   end
 end
@@ -9,7 +9,7 @@ end
 local _end_action_steelsight_original = PlayerStandard._end_action_steelsight
 function PlayerStandard:_end_action_steelsight(...)
   _end_action_steelsight_original(self, ...)
-  if ThirdPerson.settings.first_person_on_steelsight then
+  if self._unit == ThirdPerson.fp_unit and ThirdPerson.settings.first_person_on_steelsight then
     self._unit:camera():set_third_person()
   end
 end
